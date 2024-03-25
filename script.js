bfc0caba169806eb43a0db2486724f54e3da8b0f
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2024-03-25 08:21:42 +0700

    membuat variabel untuk mendapatkan value input

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ const Gameboard = (() => {
 
 })();
 
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
 const gamePlay = (() => {
+    let player1 = document.querySelector('#player1');
+    let player2 = document.querySelector('#player2');
     let players = [];
     let currentPlayerIndex;
     let gameOver;
 
     
+
+
 })();
