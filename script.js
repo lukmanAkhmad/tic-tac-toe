85a7abee7db77f4914be5e4caafcb105f20b0d8d
lukmanAkhmad <145940997+lukmanAkhmad@users.noreply.github.com.>
2025-02-11 06:24:27 +0700

    feat: game board

---
+++ script.js
@@ -0,0 +1,43 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+    console.log(board)
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithCellValues);
+    }
+
+    return { printBoard };
+}
+Gameboard();
+
+function Cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addToken,
+        getValue
+    };
+};
+
+function GameController() {
+    const board = Gameboard();
+    return { board };
+};
+
+const game = GameController();
